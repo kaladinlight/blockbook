2aa6e0fef729e3ffb4076696605b0f93f79f7cc0
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-03-11 13:28:34 -0600

    migrate address contracts from protobuf

--- blockbook.go
+++ blockbook.go
@@ -53,10 +53,11 @@ var (
 	blockUntil     = flag.Int("blockuntil", -1, "height of the final block")
 	rollbackHeight = flag.Int("rollback", -1, "rollback to the given height and quit")
 
-	synchronize = flag.Bool("sync", false, "synchronizes until tip, if together with zeromq, keeps index synchronized")
-	repair      = flag.Bool("repair", false, "repair the database")
-	fixUtxo     = flag.Bool("fixutxo", false, "check and fix utxo db and exit")
-	prof        = flag.String("prof", "", "http server binding [address]:port of the interface to profiling data /debug/pprof/ (default no profiling)")
+	synchronize          = flag.Bool("sync", false, "synchronizes until tip, if together with zeromq, keeps index synchronized")
+	repair               = flag.Bool("repair", false, "repair the database")
+	fixUtxo              = flag.Bool("fixutxo", false, "check and fix utxo db and exit")
+	migrateAddrContracts = flag.Bool("migrateAddrContracts", false, "migrate addrContracts and exit")
+	prof                 = flag.String("prof", "", "http server binding [address]:port of the interface to profiling data /debug/pprof/ (default no profiling)")
 
 	syncChunk   = flag.Int("chunk", 100, "block chunk size for processing in bulk mode")
 	syncWorkers = flag.Int("workers", 8, "number of workers to process blocks in bulk mode")
@@ -183,6 +184,14 @@ func mainWithExitCode() int {
 	if err != nil {
 		glog.Error("internalState: ", err)
 		return exitCodeFatal
+	}
+
+	if *migrateAddrContracts {
+		err = index.MigrateAddrContracts(chanOsSignal)
+		if err != nil {
+			glog.Error("migrateAddrContracts: ", err)
+			return exitCodeFatal
+		}
 	}
 
 	// fix possible inconsistencies in the UTXO index
